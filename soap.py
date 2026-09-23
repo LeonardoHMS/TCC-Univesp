906c42b09562851cf161a572b66943ccf3e173f0
Leonardo Mantovani <leorique2010@hotmail.com>
2026-09-23 09:40:52 -0300

    API Get Estados

--- soap.py
+++ soap.py
@@ -6,11 +6,19 @@ def get_data_totals(serial_code: str, format_data: str, initial_date: str, end_d
     URL_API_TOTALS = "https://api.bcb.gov.br/dados/serie/bcdata.sgs"
     url_api_totals_finish = f'{URL_API_TOTALS}.{serial_code}/dados?formato={format_data}&dataInicial={initial_date}&dataFinal={end_date}'
     json_data = requests.get(url_api_totals_finish).json()
+    print(json_data)
 
-    with open(f'{file_name}.json', 'w') as f:
+    with open(f'data/{file_name}.json', 'w') as f:
         json.dump(json_data, f, indent=4)
 
 if __name__ == "__main__":
-    get_data_totals("21082", "json", "01/01/2023", "01/07/2026", "inadimplencia_credito_total")
-    get_data_totals("21084", "json", "01/01/2023", "01/07/2026", "inadimplencia_credito_total_pf")
-    get_data_totals("21119", "json", "01/01/2023", "01/07/2026", "inadimplencia_credito_total_livres")
+    with open('estados.json', 'r', encoding='utf-8') as f:
+        estados = json.load(f)
+    for estado in estados['Estados'].values():
+        print(estado)
+        serial_code = estado
+        format_data = 'json'
+        initial_date = '01/01/2010'
+        end_date = '31/12/2025'
+        file_name = f'data_{estado}'
+        get_data_totals(serial_code, format_data, initial_date, end_date, file_name)
